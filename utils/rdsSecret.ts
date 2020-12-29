ae7ba8f6edc51018268333c940a83a8654ac6d61
chinsung 200713 <chin_sung@naver.com>
2020-12-29 20:23:58 +0900

    Create rdsSecret.ts

---
+++ utils/rdsSecret.ts
@@ -0,0 +1,8 @@
+const config = {
+  host: 'rds.cmnkfayymxyz.ap-northeast-2.rds.amazonaws.com',
+  user: 'helpetuser',
+  password: '5uperhelpet!',
+  database: 'helpet',
+};
+
+export default config;
