e10bbe1ada630cc964cbd656355746ba71410f75
chinsung 200713 <chin_sung@naver.com>
2020-12-30 21:19:35 +0900

    feat selectArticleList, selectArticle

--- utils/myDAO.ts
+++ utils/myDAO.ts
@@ -14,7 +14,7 @@ const selectArticleList = async (
     console.log(page, category_code);
 
     const query = `SELECT aidx, title, summary, thumbnail, count_view, count_like, insert_date, update_date, insert_uidx, category_code  FROM helpet.article WHERE category_code LIKE ? AND title LIKE ? ORDER BY aidx DESC LIMIT ?, ?`;
-    const queryArgs = [category_code, keyword, 10 * (page - 1), 3];
+    const queryArgs = [category_code, keyword, 10 * (page - 1), 10];
 
     const result = ((await connection.query(
       query,
@@ -28,11 +28,23 @@ const selectArticleList = async (
   });
 };
 
-const selectArticle = (aidx: number) => {
-  const query = `SELECT * FROM helpet.article WHERE aidx = ?`;
-  const queryArgs = [aidx];
+const selectArticle = async (aidx: number) => {
+  return await new Promise(async (resolve) => {
+    const connection = await pool.getConnection();
 
-  //   return databaseConnection.query(query, queryArgs);
+    const query = `SELECT aidx, title, summary, content, thumbnail, count_view, count_like, insert_date, update_date, insert_uidx, category_code  FROM helpet.article WHERE aidx = ?`;
+    const queryArgs = [aidx];
+
+    const result = ((await connection.query(
+      query,
+      queryArgs
+    )) as mysql2.RowDataPacket[][])[0][0];
+    console.log(result);
+
+    await connection.release();
+
+    resolve({ ...result, insert_date: result.insert_date.toString() });
+  });
 };
 
 export { pool, selectArticle, selectArticleList };
