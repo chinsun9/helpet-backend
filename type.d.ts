21b0da84c184d97822252905ee34097f6fc9bdb8
chinsung 200713 <chin_sung@naver.com>
2021-02-18 18:14:45 +0900

    refactor Split File

---
+++ type.d.ts
@@ -0,0 +1,25 @@
+type Article = {
+  id: number;
+  title: string;
+  datetime: string;
+  thumbnailUrl: string;
+};
+
+type ArticleInput = {
+  title: string;
+  datetime: string;
+  thumbnailUrl: string;
+};
+
+type UserInput = {
+  email: String;
+  password: String;
+  cookie: String;
+};
+
+type User = {
+  id: Number;
+  email: String;
+  password: String;
+  cookie: String;
+};
